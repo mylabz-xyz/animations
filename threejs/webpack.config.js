4ba61633243b9bf7f2c3f1cbdd70d483c95395d0
NOPR9D <contact@boucham-amine.fr>
2022-06-18 23:14:19 +0200

    :construction: : hello config

---
+++ threejs/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+var LiveReloadPlugin = require("webpack-livereload-plugin");
+module.exports = {
+  entry: "./index.ts",
+  mode: "development",
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [new LiveReloadPlugin({ port: 4200 })],
+};
